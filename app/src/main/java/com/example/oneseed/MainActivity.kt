c153d46eb9857197c9413e74017a67804da5788d
sssofi0101 <55346176+sssofi0101@users.noreply.github.com>
2022-06-09 14:32:35 +0300

    added all basic elements on 1st and added 2nd activity

--- app/src/main/java/com/example/oneseed/MainActivity.kt
+++ app/src/main/java/com/example/oneseed/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.oneseed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.findViewById<Button>(R.id.create_button_main).setOnClickListener{
+            val intent=Intent(this,CreateActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
