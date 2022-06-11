19edeb1229517730751183e6da17eb96a58f3724
sssofi0101 <55346176+sssofi0101@users.noreply.github.com>
2022-06-11 12:25:48 +0300

    +иконки, поменяла id, заголовок в CreateActivity
    Создала ветку с момента до gps, поменяла id всех элементов по смыслу, добавила ресурсы иконок, сделала кнопку Назад на 2й активити рабочей, причесала область заголовка во 2й активити.

--- app/src/main/java/com/example/oneseed/CreateActivity.kt
+++ app/src/main/java/com/example/oneseed/CreateActivity.kt
@@ -1,11 +1,19 @@
 package com.example.oneseed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        val back_button = findViewById<Button>(R.id.return_to_main_button)
+        back_button.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
