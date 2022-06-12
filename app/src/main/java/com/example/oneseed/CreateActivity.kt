847f3cd63d838a97e17934e9fe78d878f747e986
BadLog1n <54963637+BadLog1n@users.noreply.github.com>
2022-06-12 17:24:30 +0300

    Исправлено: переключение между окнами
    При переключении между activity предыдущие activity сохранялись в памяти и не закрывались.
    Теперь возврат назад возможен только из CreateActivity. MainActivity предлагает выход из приложения.

--- app/src/main/java/com/example/oneseed/CreateActivity.kt
+++ app/src/main/java/com/example/oneseed/CreateActivity.kt
@@ -1,18 +1,20 @@
 package com.example.oneseed
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
-        val back_button = findViewById<Button>(R.id.return_to_main_button)
-        back_button.setOnClickListener {
-            var intent = Intent(this, MainActivity::class.java)
+        val backButton = findViewById<Button>(R.id.return_to_main_button)
+        backButton.setOnClickListener {
+            finishAndRemoveTask()
+            Thread.sleep(50)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
