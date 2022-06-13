7ba5df8970e2b6094108f2ff400c2556357b5bf8
BadLog1n <54963637+BadLog1n@users.noreply.github.com>
2022-06-13 10:18:58 +0300

    Чистка кода, исправление ошибок
    Добавлен импорт intent
    Добавлена недостающая фигурная скобка

--- app/src/main/java/com/example/oneseed/MainActivity.kt
+++ app/src/main/java/com/example/oneseed/MainActivity.kt
@@ -3,20 +3,20 @@ package com.example.oneseed
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        this.findViewById<Button>(R.id.create_button_main).setOnClickListener{
-            
-            val intent=Intent(this,CreateActivity::class.java)
+        this.findViewById<Button>(R.id.create_button_main).setOnClickListener {
+
+            val intent = Intent(this, CreateActivity::class.java)
             startActivity(intent)
         }
     }
@@ -33,18 +33,18 @@ class MainActivity : AppCompatActivity() {
             }
             builder.setNegativeButton("Нет") { _, _ -> }
 
-        val alertDialog = builder.create()
-        alertDialog.show()
+            val alertDialog = builder.create()
+            alertDialog.show()
 
-        val button = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE)
-        with(button) {
-            setTextColor(Color.GREEN)
-        }
-        val button1 = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE)
-        with(button1) {
-            setTextColor(Color.RED) }
-        }
-        catch (e: Exception){
+            val button = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE)
+            with(button) {
+                setTextColor(Color.GREEN)
+            }
+            val button1 = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE)
+            with(button1) {
+                setTextColor(Color.RED)
+            }
+        } catch (e: Exception) {
             Toast.makeText(this, "Ошибка", Toast.LENGTH_SHORT).show()
         }
     }
