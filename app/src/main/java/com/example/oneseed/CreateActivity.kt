6788f87c590ff40468937967c9bfa277c12ea2ad
Stepan Nemchenko <54963637+BadLog1n@users.noreply.github.com>
2022-06-13 10:13:12 +0300

    Revert "Gps"

--- app/src/main/java/com/example/oneseed/CreateActivity.kt
+++ app/src/main/java/com/example/oneseed/CreateActivity.kt
@@ -1,28 +1,11 @@
 package com.example.oneseed
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.DialogInterface
-import android.content.pm.PackageManager
-import android.graphics.Color
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import java.lang.Exception
 
-class CreateActivity : AppCompatActivity() , LocationListener {
-    private lateinit var locationManager: LocationManager
-    private lateinit var tvGpsLocation: TextView
-    private val locationPermissionCode = 2
+class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
@@ -33,72 +16,6 @@ class CreateActivity : AppCompatActivity() , LocationListener {
             Thread.sleep(50)
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-
-        val button: Button = findViewById(R.id.pin_location_button)
-        button.setOnClickListener {
-            tvGpsLocation = findViewById(R.id.location_textView)
-            tvGpsLocation.text = "Определение..."
-            getLocation()
         }
     }
-
-    private fun getLocation() {
-        try {
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        if ((ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        ) {
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                locationPermissionCode)
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5, 1f, this)
-        }
-        catch (e: Exception){
-            createSimpleDialog()
-        }
-    }
-
-
-    private fun createSimpleDialog() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Ошибка в определении местоположения")
-        builder.setMessage("Чтобы мы смогли точно определять Ваше местоположение, необходимо предоставить разрешение " +
-                "на использование местоположения. Зайдите в настройки приложения " +
-                "и разрешите доступ к местоположению")
-        builder.setPositiveButton("Понятно") { _, _ -> }
-
-        val alertDialog = builder.create()
-        alertDialog.show()
-
-        val button = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE)
-        with(button) {
-            setTextColor(Color.GREEN)
-
-        }
-    }
-
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == locationPermissionCode) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Разрешение получено", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                Toast.makeText(this, "Нет доступа к местоположению", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    @SuppressLint("SetTextI18n")
-    override fun onLocationChanged(location: Location) {
-        tvGpsLocation = findViewById(R.id.location_textView)
-        var lant = location.latitude.toString()
-        lant = lant.take(6)
-        var long = location.longitude.toString()
-        long = long.take(6)
-        tvGpsLocation.text = "$lant $long"
-        locationManager.removeUpdates(this)
-    }
-
 }
