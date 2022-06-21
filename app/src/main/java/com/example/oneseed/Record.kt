2b7f11b40cecfcedd3decafae5a4ee9278c33244
sssofi0101 <55346176+sssofi0101@users.noreply.github.com>
2022-06-21 13:55:08 +0300

    Добавила класс данных одной записи, создала свой..
    RecyclerView Adapter, сделала View Holder

---
+++ app/src/main/java/com/example/oneseed/Record.kt
@@ -0,0 +1,3 @@
+package com.example.oneseed
+
+data class Record(val imageId: Int, val name: String, val dateTime:String, val yield: String, val adress: String)
---
+++ app/src/main/java/com/example/oneseed/RecordAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.oneseed
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.oneseed.databinding.RecordsItemBinding
+
+class RecordAdapter:RecyclerView.Adapter<RecordAdapter.RecordHolder>() {
+
+    val recordsList = ArrayList<Record>()
+    class RecordHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+        val binding = RecordsItemBinding.bind(itemView)
+        fun bind(record: Record) = with(binding){
+            recordStatusImg.setImageResource(record.imageId)
+            recordName.text = record.name
+            yieldInRecord.text = record.yield
+            recordDateTime.text = record.dateTime
+            recordAdress.text = record.adress
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.records_item, parent, false)
+        return RecordHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecordHolder, position: Int) {
+        holder.bind(recordsList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return recordsList.size
+    }
+
+    fun addRecord(record: Record){
+        recordsList.add(record)
+        notifyDataSetChanged()
+    }
+}
